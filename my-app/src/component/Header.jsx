c84540b93045969e32930e831ad2dc07e697ba52
ezekielysunday <62944745+ezekielysunday@users.noreply.github.com>
2023-03-03 04:52:55 +0100

    pro

---
+++ my-app/src/component/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar, Nav, Container, NavbarBrand } from 'react-bootstrap'
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        proshop
+      </nav>
+
+    </header>
+  )
+}
+
+export default Header
